fb49fa55b832960fe359634f16d6b3758290490f
Madhumitha <madhumithavenu03@gmail.com>
2023-10-05 14:41:39 +0530

    Fetching Employee details

--- index.js
+++ index.js
@@ -52,3 +52,14 @@ app.get('/employee1', (req, res) => {
         res.send('Employee Added');
     })
 })
+
+app.get('/getemployee', (req, res) => {
+    let sql = 'SELECT * FROM employee';
+    db.query(sql, (err, result) => {
+        if (err) {
+            return err;
+        }
+        console.log(result);
+        res.send('Employee details fetched');
+    })
+})
